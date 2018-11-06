525778005a30ba1cb771d907a430a8c3252b2f06
Débora FM <debofm@gmail.com>
2018-11-06 10:47:42 +0000

    uPLOAD pROJECTw

---
+++ MageMystery/MageMystery/Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    
+    public Transform player;
+    public bool LookAtPlayer = false;
+    public bool RotateAroundPlayer = false;
+
+
+    [Range(0.01f, 1.0f)]
+    public float smoothSpeed = 0.5f;
+
+    [Range(1.0f, 5.0f)]
+    public float rotationSpeed;
+
+    
+    public Vector3 _cameraOffset;
+
+    private void Start()
+    {
+        _cameraOffset = transform.position - player.position;
+    }
+
+
+    private void LateUpdate()
+    {
+
+        if(RotateAroundPlayer)
+        {
+            if (Input.GetKeyDown(KeyCode.C))
+            { 
+                Quaternion camTurnAngle = Quaternion.AngleAxis((transform.rotation.x -90f), player.transform.up);
+                _cameraOffset = camTurnAngle * _cameraOffset;
+            }
+
+            if (Input.GetKeyDown(KeyCode.X))
+            {
+                Quaternion camTurnAngle = Quaternion.AngleAxis((transform.rotation.x + 90f), player.transform.up);
+                _cameraOffset = camTurnAngle * _cameraOffset;
+            }
+        }
+
+        Vector3 newPos = player.position + _cameraOffset;
+        transform.position = Vector3.Slerp(transform.position, newPos, smoothSpeed);
+
+        if (LookAtPlayer || RotateAroundPlayer)
+        {
+            transform.LookAt(player);
+        }
+    }
+
+
+}
+
+
---
+++ MageMystery/MageMystery/Assets/Scripts/ObjectExamineBehaviour.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectExamineBehaviour : MonoBehaviour {
+
+    public enum ACTIONS { NONE, DESTROY }
+
+    public bool trigger = false;
+    public GameObject example;
+
+    private ACTIONS action = ACTIONS.NONE;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            // trigger = true;
+
+            if (action != ACTIONS.NONE)
+            {
+                Destroy(example);
+                action = ACTIONS.NONE;
+            }
+
+            Debug.Log("Made a Collission");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            trigger = false;
+            Debug.Log("Left");
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Clicked");
+
+        //if (trigger == true)
+        //{
+        //    Destroy(example);
+
+        //}
+
+        action = ACTIONS.DESTROY;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
